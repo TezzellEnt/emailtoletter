589c0ad5b7859c25704cd14cd2b41c0381abaaa7
Chris Hudson <hudsonchrisj@gmail.com>
2014-08-14 02:59:59 -0700

    add devise users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Emailtoletter::Application.routes.draw do
 
+  devise_for :users, path: "auth", path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'signup' }
+
   root to: 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

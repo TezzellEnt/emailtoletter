77fbeaf3d7a7279174c080c115391026a3839dd0
Chris Hudson <chris@cjhudson.com>
2014-08-29 06:44:40 -0700

    Orders#new to Root

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Emailtoletter::Application.routes.draw do
   resources :charges
   resources :orders
 
-  root to: 'pages#home'
+  root to: 'orders#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   # You can have the root of your site routed with "root"
